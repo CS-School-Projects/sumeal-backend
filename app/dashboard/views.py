17ce60ef512054fd710a81ab3823ab424a7b98ff
Ernest Larbi <elokai6175@gmail.com>
2022-03-27 13:55:29 +0000

    change view to CBV

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
-
-def index(request):
-    return render(request,'index.html')
+class IndexView(View):
+    template_name = 'index.html'
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
+        
