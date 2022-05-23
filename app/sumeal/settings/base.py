cda66a84da90964561a7e9cb4872035ccc15520c
Raynard Dodzi Helegah <dodzireynard@gmail.com>
2022-05-23 14:27:00 +0000

    Make an app in Django for website

--- app/sumeal/settings/base.py
+++ app/sumeal/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     "knox",
     "dashboard",
+    "website",
     "rest_api",
 ]
 
--- app/website/urls.py
+++ app/website/urls.py
@@ -13,15 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.contrib import admin
-from django.urls.conf import include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dashboard/', include("dashboard.urls")),
-    path("rest_api/", include("rest_api.urls")),
-    path("", include("website.urls")),
+    path("", views.index, name="index"),
 ]
-
