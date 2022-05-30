1017d373f23a144796486543dac4398a4ce4c3c5
Desmond Goldschmidt <macbookair@MacBooks-MacBook-Air-4.local>
2022-05-30 21:06:05 +0000

    Updated Categories 👾👻

--- products/forms.py
+++ products/forms.py
@@ -5,19 +5,6 @@ from django import forms
 from products.models import Category, Product
 from django.forms import ModelForm
 
-# class CategoryForm(ModelForm):
-#     class Meta:
-#         model = Category
-#         fields = ('name','description')
-#         CategoryName = forms.CharField()
-#         Description = forms.CharField(widget=forms.Textarea)
-#         exclude = [
-#             "id",
-#             "created_at",
-#             "updated_at",
-#         ]
-
-
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
